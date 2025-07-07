b57651cbcd254f8e636b5bdeffbe22a3d0176557
Le Thi Thuy Duyen <thuyduyen3004@gmail.com>
2025-07-07 09:09:55 +0700

    add file confidata

---
+++ Gr_Ecommerce/src/test/java/project/Constraint/ConfigData.java
@@ -0,0 +1,7 @@
+package project.Constraint;
+
+public class ConfigData {
+        public static String URL = "http://14.176.232.213/mobilevn/";
+        public static String EMAIL = "leduyenduyen3004123@gmail.com";
+        public static String PASSWORD = "3042004a3A@";
+}
